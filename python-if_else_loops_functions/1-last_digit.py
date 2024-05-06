adf10ce8540b038a94c90fcfa01a79473d5bb9a6
Carole <c.zenruffinen@hotmail.com>
2024-05-06 15:18:20 +1000

    changed syntax for neg. numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last = number % 10
+
 if number < 0:
-     number *= -1
-if number % 10 > 5:
-    print(f"Last digit of {number} is {number % 10} and is greater than 5")
-elif number % 10 < 6 and number != 0:
-      print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+     last = number % -10    
+if last > 5:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+elif last < 6 and number != 0:
+      print(f"Last digit of {number} is {last} and is less than 6 and not 0")
 else:
-      print(f"Last digit of {number} is {number % 10} and is 0")
+      print(f"Last digit of {number} is {last} and is 0")
      
