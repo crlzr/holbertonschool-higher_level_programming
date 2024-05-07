571650dd63e62ffcbe0b93393f4e6112296f03de
Carole <c.zenruffinen@hotmail.com>
2024-05-07 11:12:21 +1000

    12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# multiples of 3 = Fizz
+# multiples of 5 = Buzz
+# multiples of 3 and 5 = FizzBuzz
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end ="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(number), end="")
