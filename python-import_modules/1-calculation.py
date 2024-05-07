9678e9984f26509b4419558ab47ea3d92073ef48
Carole <c.zenruffinen@hotmail.com>
2024-05-07 12:23:45 +1000

    1-calculation.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+import calculator_1
+
+a = 10
+b = 5
+
+print("{} + {} = {}".format(a, b, (a+b)))
+print("{} - {} = {}".format(a, b, (a-b)))
+print("{} * {} = {}".format(a, b, (a*b)))
+print("{} / {} = {:.0f}".format(a, b, (a/b)))
