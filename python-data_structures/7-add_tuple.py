fbf98723852c21a914ae98606278fcf6f335f6f8
Carole <c.zenruffinen@hotmail.com>
2024-05-09 14:15:20 +1000

    added variables

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple1 = tuple_a
+    new_tuple2 = tuple_b
+
     if len(tuple_a) < 2:
-        tuple_a += (0,) * (2 - len(tuple_a))
+        new_tuple1 += (0,) * (2 - len(tuple_a))
 
     if len(tuple_b) < 2:
-        tuple_b += (0,) * (2 - len(tuple_b))
+        new_tuple2 += (0,) * (2 - len(tuple_b))
 
-    result1 = tuple_a[0] + tuple_b[0]
-    result2 = tuple_a[1] + tuple_b[1]
+    result1 = new_tuple1[0] + new_tuple2[0]
+    result2 = new_tuple1[1] + new_tuple2[1]
+
     return result1, result2
+
