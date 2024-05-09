d0285c2476c2b0452693f55f211d6ba7c56e550f
Carole <c.zenruffinen@hotmail.com>
2024-05-09 12:05:42 +1000

    added spaces

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for item in row:
-            print("{:d}".format(item), end="")
+            print("{:d} ".format(item), end="")
         print()
