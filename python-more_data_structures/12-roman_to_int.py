af7f721cdd7e74c09d02c6561c0aca15a9296ee9
Carole <c.zenruffinen@hotmail.com>
2024-05-10 14:46:31 +1000

    changed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                       'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
-    if type(roman_string) != str:
+    if not isinstance(roman_string, str):
         return 0
 
     for i in range(len(roman_string)):
-
         value = roman_numerals.get(roman_string[i], 0)
-
         if (i + 1 < len(roman_string) and
-            roman_numerals.get(roman_string[i + 1], 0) > value):
-
+                roman_numerals.get(roman_string[i + 1], 0) > value):
             result -= value
         else:
-
             result += value
 
     return result
+
