76f0d543b557f68fe30e5a21143daff11859c72f
Carole <c.zenruffinen@hotmail.com>
2024-05-10 14:42:02 +1000

    no string is wrong

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
                       'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
-    if roman_string is None or len(roman_string) == 0:
+    if roman_string is None or roman_string == 0:
         return 0
 
     for i in range(len(roman_string)):
