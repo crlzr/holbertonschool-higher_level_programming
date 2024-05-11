a5048dbff727d8c70426316c091f196ed6782ef1
Carole <c.zenruffinen@hotmail.com>
2024-05-11 14:17:09 +1000

    3-safe_print_division.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    result = None
     try:
         result = a / b
-        print("{}".format(a / b))
-    except (ZeroDivisionError, TypeError, IndexError):
-        return None
+    except ZeroDivisionError:
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
