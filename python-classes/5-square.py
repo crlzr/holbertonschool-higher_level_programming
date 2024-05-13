a2303f63632d031c8aa132625b38835f8015ad0f
Carole <c.zenruffinen@hotmail.com>
2024-05-13 16:37:12 +1000

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+class Square:
+    '''A class that defines a square'''
+    def __init__(self, size=0):
+        ''' private attribute size'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Determines whether the size is appropriate'''
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        '''returns the current square area'''
+        return self.__size ** 2
+
+    def my_print(self):
+        ''' prints to stdout the square with the character #'''
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
