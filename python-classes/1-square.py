f0ea2d56002bda16681176bc455de0b141a60248
Carole <c.zenruffinen@hotmail.com>
2024-05-13 14:47:52 +1000

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""An class Square that defines a square by private instance attribute: size"""
+"""A class Square that defines a square"""
 
 class Square:
-    def __init__(self, size=0):
-        if not type(int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    """Contains a private instance atrribute size"""
+    def __init__(self, size):
+        self.__size = size
