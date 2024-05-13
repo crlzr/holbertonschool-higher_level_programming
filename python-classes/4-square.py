d3108b2879afa656e33d1049e956f73320353f6b
Carole <c.zenruffinen@hotmail.com>
2024-05-13 15:56:59 +1000

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+''' A class that defines a Square'''
+
+
+class Square:
+    def __init__(self, size=0):
+        '''Contains a private attribute called size'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''Contains an attribute called size'''
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
