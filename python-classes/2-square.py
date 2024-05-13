f544557c7ad604abe1dde4bec3fd0ad9239925d8
Carole <c.zenruffinen@hotmail.com>
2024-05-13 15:00:31 +1000

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 class Square:
     """Contains a private instance attribute size"""
     def __init__(self, size=0):
-        if not type(int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
