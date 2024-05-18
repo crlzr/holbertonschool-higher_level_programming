e5d29a1f277c28f3c46423c5dad213c1f6eabf3f
Carole <c.zenruffinen@hotmail.com>
2024-05-18 15:55:44 +1000

    5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """
+    Args:
+        text (str): The input text.
+    Raises:
+        TypeError: If text is not a string.
+    Prints:
+        The modified text with 2 new lines after each '.', '?', and ':'.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+    i = 0
+    while i < len(text):
+        result += text[i]
+        if text[i] in {'.', '?', ':'}:
+            result += "\n\n"
+            while i + 1 < len(text) and text[i + 1] == ' ':
+                i += 1
+        i += 1
+
+    lines = result.split('\n')
+    stripped_lines = [line.strip() for line in lines]
+    cleaned_result = '\n'.join(stripped_lines)
+
+    print(cleaned_result)
