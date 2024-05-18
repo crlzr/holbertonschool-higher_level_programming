a565c6f60ae1ec6196a4bd115a5c3e31a32c2c2c
Carole <c.zenruffinen@hotmail.com>
2024-05-18 17:42:29 +1000

    added an a in text

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,8 +11,8 @@ def say_my_name(first_name, last_name=""):
     Return: prints "My name is <first_name> <last_name>
     """
     if type(first_name) is not str:
-        raise TypeError("first_name must be string")
+        raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
-        raise TypeError("last_name must be string")
+        raise TypeError("last_name must be a string")
     else:
         print(f"My name is {first_name} {last_name}")
