a6a99363f0a645838b37a2629bd321ba7fa4a939
Carole <c.zenruffinen@hotmail.com>
2024-05-21 19:37:41 +1000

    changed function back

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -23,10 +23,9 @@ class Rectangle(BaseGeometry):
     Subclass Rectangle
     """
     def __init__(self, width, height):
-        """ Initializes instance """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
 
 
