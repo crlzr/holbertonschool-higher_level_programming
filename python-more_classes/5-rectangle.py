696413a45bf9733edd97026b987c4e6f8d2e1963
Carole <c.zenruffinen@hotmail.com>
2024-05-21 15:44:26 +1000

    5-rectangle.py

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+Module for rectancle class
+"""
+
+
+class Rectangle:
+    """
+    A class Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Attributes width and height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        returns the product of width and height
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        returns the sum of all sides of a rectangle if width or height is not 0
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        Prints the rectangle with the character #
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle_lines = ["#" * self.__width] * self.__height
+        return "\n".join(rectangle_lines)
+
+    def __repr__(self):
+        """
+        return a string representation of the rectangle
+        with which we can recreate a new instance by using eval()
+        """
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
