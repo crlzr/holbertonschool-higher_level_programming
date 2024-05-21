2b3c55cc89a89100b8abc01bbd67510af896012f
Carole <c.zenruffinen@hotmail.com>
2024-05-21 19:59:01 +1000

    changed functions

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""
-Nameless module
-"""
+""" Nameless module containing BaseGeometry class """
 
 
-class BaseGeometry:
-    """
-    class BaseGeometry
+class BaseGeometry():
+    """BaseGeometry Class.
     """
 
     def area(self):
+        """returns area"""
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        """validates value"""
 
+        if type(value) is not int:
+            raise TypeError("{0} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{0} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
-    """
-    Subclass Rectangle
-    """
     def __init__(self, width, height):
-        super().__init__()
         self.__width = width
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
