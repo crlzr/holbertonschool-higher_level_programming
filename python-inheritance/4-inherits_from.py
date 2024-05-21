e06abb5b2e3f79584529c47f9e210a9a56bc28b9
Carole <c.zenruffinen@hotmail.com>
2024-05-21 18:46:09 +1000

    changed function

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-???
+Nameless module
 """
 
 
 def inherits_from(obj, a_class):
-    if isinstance(obj, a_class):
-        return True
-    else:
+    """
+    Returns True if object is an instance of a class that
+    inherited from the specified class, else False
+    """
+    if type(obj) is a_class:
         return False
+    else:
+        return isinstance(obj, a_class)
