437b181bdbc0bb02b20baa0d76d45481cb798a2c
Carole <c.zenruffinen@hotmail.com>
2024-05-21 18:57:53 +1000

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Nameless module
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
