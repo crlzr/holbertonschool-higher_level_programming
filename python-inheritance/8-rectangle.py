4efe4b52c5bcf07d370c5a94e950dd5ca40e9704
Carole <c.zenruffinen@hotmail.com>
2024-05-21 19:31:11 +1000

    changing things

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,7 +13,6 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        self.name = value
         if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
         if value <= 0:
