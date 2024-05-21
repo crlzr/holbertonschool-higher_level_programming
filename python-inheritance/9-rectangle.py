fb1fd844a3d32e64c5640f0fc776b4c7d24f39c6
Carole <c.zenruffinen@hotmail.com>
2024-05-21 20:51:43 +1000

    pycodeestyle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -6,10 +6,8 @@ class BaseGeometry():
     """BaseGeometry Class.
     """
 
-
     def integer_validator(self, name, value):
         """validates value"""
-
         if type(value) is not int:
             raise TypeError("{0} must be an integer".format(name))
 
@@ -27,14 +25,12 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-
     def area(self):
         """returns area"""
-
         return (self.__width * self.__height)
 
     def __str__(self):
         """
         returns a string representation
         """
-        return(f"[Rectangle] {self.__width}/{self.__height}")
+        return (f"[Rectangle] {self.__width}/{self.__height}")
