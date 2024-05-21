3e402fcd2b58f55fc89bcdc7a5f7f1d88421255c
Carole <c.zenruffinen@hotmail.com>
2024-05-21 18:32:43 +1000

    added module docstring

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,11 @@ Nameless module
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Returns True if object is instance of or instance of
+    a class that inherited from the specified class,
+    else False
+    """
     if isinstance(obj, a_class):
         return True
     else:
