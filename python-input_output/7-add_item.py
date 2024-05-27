51269013ec4c108d6b55dee5be9b14392968eb6a
Carole <c.zenruffinen@hotmail.com>
2024-05-27 20:31:15 +1000

    pycodestyle

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,14 +10,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
-# Try to load the existing list from the file, or create a new list if the file doesn't exist
 if os.path.exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
-# Add all command-line arguments to the list (excluding the script name itself)
 items.extend(sys.argv[1:])
 
-# Save the updated list to the file
 save_to_json_file(items, filename)
