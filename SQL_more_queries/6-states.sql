a5d4c5f4cf2140072f997bc2c07605399a0a74f9
Carole <c.zenruffinen@hotmail.com>
2024-06-18 14:05:08 +1000

    task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- create db hbtn_0d_usa and table states
+-- id INT, unique, auto generated, can't be null, primary key
+-- script shouldn't fail if db or table already exists
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT, UNIQUE IS NOT NULL PRIMARY KEY UNIQUE,
+    name VARCHAR(256) IS NOT NULL
+);
