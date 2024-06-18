73e20ffba5ceb24e45da4778d5f2aeb9e1571cae
Carole <c.zenruffinen@hotmail.com>
2024-06-18 10:28:12 +1000

    deleted database, changed order of score and name

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- list all records >= 10 of the second_table, show name and best score first
 
-SELECT name, score FROM hbtn_0c_0.second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
