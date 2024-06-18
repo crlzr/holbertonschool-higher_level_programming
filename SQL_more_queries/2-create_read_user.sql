178ccf305c6abb256ba92195d265096b5f19d49a
Carole <c.zenruffinen@hotmail.com>
2024-06-18 11:32:09 +1000

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- script should not fail if db or user already exists
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT PRIVILEGE ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
