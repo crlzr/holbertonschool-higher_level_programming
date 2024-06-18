bcdf63bd63ccccd2519dff020e4d38ff39cb50dc
Carole <c.zenruffinen@hotmail.com>
2024-06-18 10:25:12 +1000

    deleted database from code

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- list all records of the second_table, show name and best score first
 
-SELECT name, score FROM hbtn_0c_0.second_table ORDER BY score DESC;
+SELECT name, score FROM second_table ORDER BY score DESC;
