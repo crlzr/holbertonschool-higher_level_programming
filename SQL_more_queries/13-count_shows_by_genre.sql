85bdc22e85a4d6ae0cd6e58ca0ea3e6578cd36e0
Carole <c.zenruffinen@hotmail.com>
2024-06-19 07:52:06 +1000

    task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,9 @@
 -- list all genres and display the number of shows linked to each
 
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_shows.id) AS number_of_shows
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows ASC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
