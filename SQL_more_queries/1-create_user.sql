2016f507e0fca1d7bf66aa08d4bc783e1c188637
Carole <c.zenruffinen@hotmail.com>
2024-06-18 11:16:17 +1000

    added password portion

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create user user_0d_1 / all privileges / pw: user_0d_1_pwd
 -- if user already exists, script should not fail
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
