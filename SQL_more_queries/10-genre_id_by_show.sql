c2cbcf504a59a844374574c79084f348655b281a
Carole <c.zenruffinen@hotmail.com>
2024-06-18 17:49:49 +1000

    changed the ON statement

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,6 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
-ON tv_shows.title = tv_show_genres.genres_id
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
