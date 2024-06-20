cea0d82791faf29b315712e92526fb66cb00f43b
Carole <c.zenruffinen@hotmail.com>
2024-06-20 12:13:02 +1000

    indentation error

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,20 @@ Script that lists all states from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
-import sys
-import MySQLdb
+    import sys
+    import MySQLdb
 
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
 
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3]
-                     )
+    cur = db.cursor()
 
-cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
 
-cur.execute("SELECT * FROM states ORDER BY id")
-rows = cur.fetchall()
-
-for row in rows:
-    print (row)
+    for row in rows:
+        print(row)
