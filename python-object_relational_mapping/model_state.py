69653cc49a37499af241ccc7b85bade1fadf54a7
Carole <c.zenruffinen@hotmail.com>
2024-06-21 09:11:19 +1000

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,7 +3,6 @@
 """
 
 import sys
-from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
