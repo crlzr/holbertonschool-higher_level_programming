4af4d09ea6edc05766c44af244847015fde7f6ce
Carole <c.zenruffinen@hotmail.com>
2024-06-20 17:06:51 +1000

    3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument - SQL injection safe
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> \
+              <database> <state>".format(sys.argv[0]))
+        sys.exit(1)
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+
+    query = """SELECT * FROM states
+            WHERE BINARY name = '{}'
+            ORDER BY id ASC""".format(sys.argv[4])
+
+    cur = db.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
