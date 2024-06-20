3901b5be055d4bb2323da826c74d92d4c5b8a145
Carole <c.zenruffinen@hotmail.com>
2024-06-21 08:06:30 +1000

    pycodestyle

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
     import sys
     import MySQLdb
 
-
     db = MySQLdb.connect(host='localhost',
                          port=3306,
                          user=sys.argv[1],
