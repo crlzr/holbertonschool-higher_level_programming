8768c8648570b29f9c455c38ad1272c9883c4ef2
Carole <c.zenruffinen@hotmail.com>
2024-06-21 11:06:44 +1000

    task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
+"""Nameless module to suck data out from the database
 """
-Write a script that prints the State object with the name
-passed as argument from the database hbtn_0e_6_usa"""
-
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for av in sys.argv:
-        if av.count(";") > 0:
-            exit()
+    state_name = sys.argv[4]
 
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]),
-            pool_pre_ping=True
-        )
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    query = session.query(State).where(State.name == state_name)
 
-        state_name = sys.argv[4]
-
-        query = session.query(State).filter(State.name == state_name).limit(1).first()
-
-        if query:
-            print(query.id)
-        else:
-            print("Not found")
+    if query.count() == 0:
+        print("Not found")
+    else:
+        row = query.limit(1).one()
+        print("{0}".format(row.id))
