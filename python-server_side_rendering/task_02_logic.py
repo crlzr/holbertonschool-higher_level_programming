02efd570891597fe4115ad8378da5e3285a29dd3
Carole <c.zenruffinen@hotmail.com>
2024-07-15 17:00:29 +1000

    restructering of files and changing code to pass checker

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-""" Set up a Flask environment and create a basic Flask app"""
-
-import json
+"""Basic flask application that renders a HTML template"""
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    """ renders a HTML template """
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/items")
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
 def items():
-    """ renders a HTML template """
+    # Read and and load items from json file to python dict
+    with open('items.json') as f:
+        data = json.load(f)
+    # retrieve values of "items" key
+    # assign empty list if 'items' not present
+    items = data.get('items', [])
 
-    items_list = []
-
-    with open("./data/items.json", 'r') as f:
-        rows = json.load(f)
-    for key,value in rows.items():
-
-        items_list = value
-    return render_template('items.html', items=items_list)
-
+    # Put items into html template
+    return render_template('items.html', items=items)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True, port=5000)
