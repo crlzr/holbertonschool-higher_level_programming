a7b19d254b1cd720b559b094dd133e291b1d6c12
Carole <c.zenruffinen@hotmail.com>
2024-07-16 16:13:30 +1000

    made changes to the loop

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,4 +1,5 @@
-/* fetch  and list the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json
+/* fetch  and list the title for all movies by using this URL:
+https://swapi-api.hbtn.io/api/films/?format=json
 All movie titles must be list in the HTML ul element with id list_movies
 You must use the Fetch API.
 . */
@@ -7,14 +8,15 @@ function getTitle() {
   fetch('https://swapi-api.hbtn.io/api/films/?format=json')
     .then(response => response.json())
     .then((data) => {
-      data.results.forEach((film) => {
-      const list = document.getElementById('list_movies');
-      const li = document.createElement("li");
-      li.textContent = film.title;
-      list.appendChild(li);
-      });
+      for (let movie of data.results) {
+        const list = document.getElementById('list_movies');
+        const li = document.createElement("li");
+        li.textContent = movie.title;
+        list.appendChild(li);
+      }
     });
 }
 
 // Call the function to fetch and display the titles
 getTitle();
+
